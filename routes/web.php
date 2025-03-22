59edbde8c6ee9a077e3736951e386db76abb7b36
cleberjesus2 <cleberjesus493@gmail.com>
2025-03-22 17:55:54 -0300

    Adicionando rotas para Redacao e seus metodos

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SimuladoCoringaController;
 use App\http\Controllers\SimuladoComumController;
+use App\Http\Controllers\RedacaoController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,12 +18,18 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
 });
 
-Route::get('/simulado-coringa', [SimuladoCoringaController::class, 'index'])->name('simulado-coringa'); 
-Route::get('/simulado-coringa/gerar-tema', [SimuladoCoringaController::class, 'gerarTema'])->name('simulado-coringa.gerarTema');
-Route::get('/simulado/finalizar', [SimuladoCoringaController::class, 'finalizarSimulado'])->name('simulado.finalizar');
-Route::get('/simulado-comum', [SimuladoComumController::class, 'index'])->name('simulado-comum'); 
 
+Route::get('/simulado-coringa', [SimuladoCoringaController::class, 'index'])->name('simulado-coringa');
+Route::get('/simulado-coringa/gerar-tema', [SimuladoCoringaController::class, 'gerarTema'])->name('simulado-coringa.gerarTema');
+
+Route::get('/simulado/finalizar', [SimuladoCoringaController::class, 'finalizarSimulado'])->name('simulado.finalizar');
+Route::get('/simulado-comum', [SimuladoComumController::class, 'index'])->name('simulado-comum');
+
+
+Route::get('/redacoes', [RedacaoController::class, 'index'])->name('redacoes.index');
+Route::post('/redacoes', [RedacaoController::class, 'store'])->name('redacoes.store');
 
 require __DIR__.'/auth.php';
