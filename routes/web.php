621a3e3735c458d4f2adc9c8f86932edc2d74880
cleberjesus2 <cleberjesus493@gmail.com>
2025-03-22 12:06:56 -0300

    Adicionando Rota para o  Simulado Coringa

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SimuladoCoringaController;
+use App\http\Controllers\SimuladoComumController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,7 +19,11 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/simulado-coringa', [SimuladoCoringaController::class, 'index'])->name('simulado-coringa');
+Route::get('/simulado-coringa', [SimuladoCoringaController::class, 'index'])->name('simulado-coringa'); 
+Route::get('/simulado-coringa/gerar-tema', [SimuladoCoringaController::class, 'gerarTema'])->name('simulado-coringa.gerarTema');
+
+Route::get('/simulado/finalizar', [SimuladoCoringaController::class, 'finalizarSimulado'])->name('simulado.finalizar');
+Route::get('/simulado-comum', [SimuladoComumController::class, 'index'])->name('simulado-comum');
 
 
 require __DIR__.'/auth.php';
