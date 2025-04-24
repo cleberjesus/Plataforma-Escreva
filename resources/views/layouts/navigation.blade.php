d02e9944af00d5c1000b3c043499d2f87de47f73
cleberjesus2 <cleberjesus493@gmail.com>
2025-04-23 22:29:18 -0300

    Inserindo responsividade na aba minhas redacoes

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,16 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                    <a href="{{ route('dashboard') }}">
-    <img src="/images/logo-removebg-preview.png" alt="Logo" class="h-14 w-auto max-w-[300px]">
-                    </a>
-
-
-
-
-
-
-
+                        <img src="/images/logo-removebg-preview.png" alt="Logo" class="h-14 w-auto max-w-[300px]">
                     </a>
                 </div>
 
@@ -25,32 +16,27 @@
                         {{ __('Painel') }}
                     </x-nav-link>
 
-                    <!-- Link para Simulado Coringa com Ícone -->
                     <x-nav-link :href="route('simulado-coringa')" :active="request()->routeIs('simulado-coringa')">
                         <img src="/icons/joker.png" alt="Ícone do Simulado Coringa" class="h-6 w-6 inline-block mr-1" />
                         {{ __('Simulado Coringa') }}
                     </x-nav-link>
 
-                    <!-- Link para Simulado Comum com Ícone -->
                     <x-nav-link :href="route('simulado-comum')" :active="request()->routeIs('simulado-comum')">
-                    <img src="/icons/open-book.png" alt="Ícone do Simulado Comum" class="h-6 w-6 inline-block mr-1" />
+                        <img src="/icons/open-book.png" alt="Ícone do Simulado Comum" class="h-6 w-6 inline-block mr-1" />
                         {{ __('Simulado Comum') }}
                     </x-nav-link>
 
                     <x-nav-link :href="route('redacoes.index')" :active="request()->routeIs('redacoes.index')">
-                    <img src="/icons/contract.png" alt="Ícone de Minhas Redações" class="h-6 w-6 inline-block mr-1" />
-                    {{ __('Minhas Redações') }}
+                        <img src="/icons/contract.png" alt="Ícone de Minhas Redações" class="h-6 w-6 inline-block mr-1" />
+                        {{ __('Minhas Redações') }}
                     </x-nav-link>
-
-                
-                     
                 </div>
             </div>
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">   
-                    <x-slot name="trigger" >
+                    <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
                             <div class="ms-1">
@@ -95,19 +81,23 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+                {{ __('Painel') }}
             </x-responsive-nav-link>
 
-            <!-- Link para Simulado Coringa no menu responsivo -->
             <x-responsive-nav-link :href="route('simulado-coringa')" :active="request()->routeIs('simulado-coringa')">
                 <img src="/icons/joker.png" alt="Ícone do Simulado Coringa" class="h-6 w-6 inline-block mr-1" />
                 {{ __('Simulado Coringa') }}
             </x-responsive-nav-link>
 
-            <!-- Link para Simulado Comum no menu responsivo -->
             <x-responsive-nav-link :href="route('simulado-comum')" :active="request()->routeIs('simulado-comum')">
-                <img src="/icons/open-book.png" alt="Ícone do Simulado Coringa" class="h-6 w-6 inline-block mr-1" />
+                <img src="/icons/open-book.png" alt="Ícone do Simulado Comum" class="h-6 w-6 inline-block mr-1" />
                 {{ __('Simulado Comum') }}
+            </x-responsive-nav-link>
+
+            <!-- Minhas Redações - responsivo -->
+            <x-responsive-nav-link :href="route('redacoes.index')" :active="request()->routeIs('redacoes.index')">
+                <img src="/icons/contract.png" alt="Ícone de Minhas Redações" class="h-6 w-6 inline-block mr-1" />
+                {{ __('Minhas Redações') }}
             </x-responsive-nav-link>
         </div>
 
