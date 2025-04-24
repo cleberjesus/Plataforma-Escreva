43e2a911ac9587bb89971637519e671ee28fe1d6
cleberjesus2 <cleberjesus493@gmail.com>
2025-04-23 22:34:12 -0300

    Logica de visualizacao dos cronogramas

--- app/Http/Controllers/CronogramaController.php
+++ app/Http/Controllers/CronogramaController.php
@@ -4,12 +4,43 @@ namespace App\Http\Controllers;
 
 use App\Models\Cronograma;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class CronogramaController extends Controller
 {
     public function index()
     {
-        $cronogramas = Cronograma::where('user_id', auth()->id())->get();
+        $cronogramas = Cronograma::with('atividades')->where('user_id', auth()->id())->get();
+
+        foreach ($cronogramas as $cronograma) {
+            $inicio = Carbon::parse($cronograma->inicio);
+            $fim = $cronograma->fim ? Carbon::parse($cronograma->fim) : now()->addMonths(1);
+            $diasSelecionados = $cronograma->dias_da_semana;
+            $diasJaMarcados = $cronograma->atividades->pluck('data')->toArray();
+
+            $mapaSemana = [
+                'domingo' => 0,
+                'segunda' => 1,
+                'terca' => 2,
+                'quarta' => 3,
+                'quinta' => 4,
+                'sexta' => 5,
+                'sabado' => 6
+            ];
+
+            $diasValidos = [];
+            for ($data = $inicio->copy(); $data->lte($fim); $data->addDay()) {
+                $diaDaSemana = $data->dayOfWeek;
+                foreach ($diasSelecionados as $dia) {
+                    if ($diaDaSemana == $mapaSemana[$dia] && !in_array($data->toDateString(), $diasJaMarcados)) {
+                        $diasValidos[] = $data->copy();
+                    }
+                }
+            }
+
+            $cronograma->dias_validos = $diasValidos;
+        }
+
         return view('cronograma.index', compact('cronogramas'));
     }
 
