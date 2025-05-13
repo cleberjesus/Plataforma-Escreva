5a469e598d78cb334a052ccae87e03f2ce012399
cleberjesus2 <cleberjesus493@gmail.com>
2025-05-12 22:19:14 -0300

    feat: Add index view for correcoes with back button and layout structure

---
+++ resources/views/correcoes/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <x-back-button />
+        
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            // ... existing code ...
+        </div>
+    </div>
+</div>
+@endsection 
