2907e16109fc246f7ed7db4d08227c59285a5ba3
cleberjesus2 <cleberjesus493@gmail.com>
2025-05-12 22:18:54 -0300

    refactor: Update achievements index layout with improved structure and added back button

--- resources/views/achievements/index.blade.php
+++ resources/views/achievements/index.blade.php
@@ -1,33 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h1>Minhas Conquistas</h1>
-    @if($achievements->isEmpty())
-        <p>Você ainda não desbloqueou nenhuma conquista.</p>
-    @else
-        <div class="row">
-            @foreach($achievements as $achievement)
-            <div class="col-md-4">
-                <div class="card mb-3">
-                    @if($achievement->icon)
-                        <img src="{{ asset($achievement->icon) }}" class="card-img-top" alt="{{ $achievement->name }}">
-                    @endif
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $achievement->name }}</h5>
-                        <p class="card-text">{{ $achievement->description }}</p>
-                        @if($achievement->pivot->achieved_at)
-                            <p class="card-text">
-                                <small class="text-muted">
-                                    Desbloqueada em: {{ \Carbon\Carbon::parse($achievement->pivot->achieved_at)->format('d/m/Y') }}
-                                </small>
-                            </p>
-                        @endif
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <x-back-button />
+        
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="container">
+                <h1>Minhas Conquistas</h1>
+                @if($achievements->isEmpty())
+                    <p>Você ainda não desbloqueou nenhuma conquista.</p>
+                @else
+                    <div class="row">
+                        @foreach($achievements as $achievement)
+                        <div class="col-md-4">
+                            <div class="card mb-3">
+                                @if($achievement->icon)
+                                    <img src="{{ asset($achievement->icon) }}" class="card-img-top" alt="{{ $achievement->name }}">
+                                @endif
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $achievement->name }}</h5>
+                                    <p class="card-text">{{ $achievement->description }}</p>
+                                    @if($achievement->pivot->achieved_at)
+                                        <p class="card-text">
+                                            <small class="text-muted">
+                                                Desbloqueada em: {{ \Carbon\Carbon::parse($achievement->pivot->achieved_at)->format('d/m/Y') }}
+                                            </small>
+                                        </p>
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
                     </div>
-                </div>
+                @endif
             </div>
-            @endforeach
         </div>
-    @endif
+    </div>
 </div>
 @endsection
