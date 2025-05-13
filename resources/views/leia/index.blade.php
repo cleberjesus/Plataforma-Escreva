38207e2fd825a39bee0bd463dbda5f8c7a8c4dc8
cleberjesus2 <cleberjesus493@gmail.com>
2025-05-12 22:21:05 -0300

    feat: Inserindo icone de voltar

--- resources/views/leia/index.blade.php
+++ resources/views/leia/index.blade.php
@@ -1,25 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-    <!-- Exibindo mensagem de erro ou sucesso -->
-    @if(isset($mensagem))
-        <div class="alert alert-info">
-            {{ $mensagem }}
-        </div>
-    @endif
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <x-back-button />
+        
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <!-- Exibindo mensagem de erro ou sucesso -->
+            @if(isset($mensagem))
+                <div class="alert alert-info">
+                    {{ $mensagem }}
+                </div>
+            @endif
 
-    <!-- Exibindo notícias -->
-    @if(count($noticias) > 0)
-        @foreach ($noticias as $noticia)
-            <div class="noticia">
-                <h3>{{ $noticia['title'] }}</h3>
-                <p>{{ $noticia['description'] }}</p>
-                <a href="{{ $noticia['url'] }}" target="_blank">Leia mais</a>
-            </div>
-        @endforeach
-    @else
-        <div class="alert alert-warning">
-            Não há notícias disponíveis no momento.
+            <!-- Exibindo notícias -->
+            @if(count($noticias) > 0)
+                @foreach ($noticias as $noticia)
+                    <div class="noticia">
+                        <h3>{{ $noticia['title'] }}</h3>
+                        <p>{{ $noticia['description'] }}</p>
+                        <a href="{{ $noticia['url'] }}" target="_blank">Leia mais</a>
+                    </div>
+                @endforeach
+            @else
+                <div class="alert alert-warning">
+                    Não há notícias disponíveis no momento.
+                </div>
+            @endif
         </div>
-    @endif
+    </div>
+</div>
 @endsection
