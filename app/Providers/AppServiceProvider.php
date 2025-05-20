59ae65db446aa2c3f40906eb9250340d7671525e
cleberjesus2 <cleberjesus493@gmail.com>
2025-05-19 21:53:25 -0300

    feat: Add NoCaptcha rule import in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,10 +3,13 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Anhskohbo\NoCaptcha\Rules\Captcha;
 
 class AppServiceProvider extends ServiceProvider
 {
+    
     /**
+     * 
      * Register any application services.
      */
     public function register(): void
