f598ecd2ec5bb3e1af2093fe1756ff7f46a34790
cleberjesus2 <cleberjesus493@gmail.com>
2025-05-25 18:51:40 -0300

    feat: Extend Redacao model with additional fields and user association

--- app/Models/Redacao.php
+++ app/Models/Redacao.php
@@ -10,8 +10,36 @@ class Redacao extends Model
     use HasFactory;
     protected $table = 'redacoes';
 
-    protected $fillable = ['tema','modo_envio', 'texto_redacao', 'imagem_redacao'];
-    protected $dates = ['created_at', 'updated_at'];
+    protected $fillable = [
+        'tema',
+        'modo_envio', 
+        'texto_redacao', 
+        'imagem_redacao',
+        'tempo_gasto',
+        'nota_comp1',
+        'nota_comp2',
+        'nota_comp3',
+        'nota_comp4',
+        'nota_comp5',
+        'nota_total',
+        'comentario',
+        'corrigida'
+    ];
     
+    protected $dates = ['created_at', 'updated_at'];
 
+    protected $casts = [
+        'corrigida' => 'boolean',
+        'nota_comp1' => 'decimal:1',
+        'nota_comp2' => 'decimal:1',
+        'nota_comp3' => 'decimal:1',
+        'nota_comp4' => 'decimal:1',
+        'nota_comp5' => 'decimal:1',
+        'nota_total' => 'decimal:1',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
