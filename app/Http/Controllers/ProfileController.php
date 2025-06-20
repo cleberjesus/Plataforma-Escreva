55b84dbe27e6cf6a8996689b96e1f3cb63871270
cleberjesus2 <cleberjesus493@gmail.com>
2025-06-20 17:33:51 -0300

    fix: Inserindo validação de senha ao alterar email e nome

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,14 +6,13 @@ use App\Http\Requests\ProfileUpdateRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Validation\ValidationException;
 use Illuminate\View\View;
 
 class ProfileController extends Controller
 {
-    /**
-     * Display the user's profile form.
-     */
     public function edit(Request $request): View
     {
         return view('profile.edit', [
@@ -21,25 +20,32 @@ class ProfileController extends Controller
         ]);
     }
 
-    /**
-     * Update the user's profile information.
-     */
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
-        $request->user()->fill($request->validated());
+        $user = $request->user();
 
-        if ($request->user()->isDirty('email')) {
-            $request->user()->email_verified_at = null;
+        // Se o e-mail for alterado, valida a senha
+        if ($request->email !== $user->email) {
+            $request->validate([
+                'current_password' => ['required'],
+            ]);
+
+            if (!Hash::check($request->current_password, $user->password)) {
+                return Redirect::route('profile.edit')->withErrors([
+                    'current_password' => 'A senha informada está incorreta.',
+                ])->withInput();
+            }
+
+            // Resetar verificação de e-mail
+            $user->email_verified_at = null;
         }
 
-        $request->user()->save();
+        $user->fill($request->validated());
+        $user->save();
 
         return Redirect::route('profile.edit')->with('status', 'profile-updated');
     }
 
-    /**
-     * Delete the user's account.
-     */
     public function destroy(Request $request): RedirectResponse
     {
         $request->validateWithBag('userDeletion', [
